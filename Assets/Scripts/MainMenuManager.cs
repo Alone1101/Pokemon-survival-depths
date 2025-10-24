5be69b34a08b43ec22af5acb1d0393ce8906b628
Alone1101 <wongjinxuan3@gmail.com>
2025-10-24 23:17:00 +0800

    Add settings menu

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -2,17 +2,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
-{
-    void Start()
-    {
-        // Debug what scenes are actually in build
-        for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++)
-        {
-            string scenePath = SceneUtility.GetScenePathByBuildIndex(i);
-            Debug.Log($"Build Index {i}: {scenePath}");
-        }
-    }
-    
+{  
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Return))
